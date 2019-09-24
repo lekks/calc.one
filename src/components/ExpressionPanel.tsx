a288a408d4f0ab7e477a06aa13fa23532091d3ac
Alexey Dolinenko <lekks@yandex.ru>
2019-09-24 20:03:31 +0300

    Add katex lib

--- src/components/ExpressionPanel.tsx
+++ src/components/ExpressionPanel.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import {Expression} from "../stores/Expression";
 
+import 'katex/dist/katex.min.css';
+import { BlockMath } from 'react-katex';
+
 
 interface Props {
     expression: Expression;
 }
 
 const ExpressionPanel: React.FC<Props> = (expression) => {
+
     return (
         <div className="Panel">
             :{expression.expression.getFormula()}={expression.expression.getResult()}
+            <BlockMath math="\dots"/>
         </div>
     );
 };
