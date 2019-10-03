c7529e5e9cc8f0457d5497e28b33362fcdc3fd61
Alexey Dolinenko <lekks@yandex.ru>
2019-10-03 07:22:18 +0300

    got rid off plain formulas

--- src/stores/Expression.ts
+++ src/stores/Expression.ts
@@ -9,23 +9,22 @@ export enum OperationRank {
 export type Result = number;
 export interface Expression {
     getResult(): Result;
-    getFormula(): string;
     getRank(): OperationRank;
     getTex(): string;
     useExplicitTexPareses(): boolean;
 }
 
 export class NumberExpression implements Expression {
-    private readonly formula: string;
+    private readonly tex: string;
     private readonly result_value: number;
 
     constructor(value: string | number) {
         if (typeof value === 'number') {
-            this.formula = value.toString();
+            this.tex = value.toString();
             this.result_value = value;
         } else {
             this.result_value = parseFloat(value);
-            this.formula = value;
+            this.tex = value;
         }
     }
 
@@ -33,16 +32,12 @@ export class NumberExpression implements Expression {
         return this.result_value;
     }
 
-    getFormula(): string {
-        return this.formula
-    }
-
     getRank(): number {
         return OperationRank.NUMBER
     }
 
     getTex(): string {
-        return this.formula;
+        return this.tex;
     }
 
     useExplicitTexPareses(): boolean {
@@ -54,7 +49,6 @@ export class NumberExpression implements Expression {
 type ArithmeticTexBuilder = (leftStr: string, rightStr: string, leftExpr: Expression, rightExpr: Expression) => string;
 
 export class ArithmeticExpression implements Expression {
-    private readonly formula: string;
     private readonly tex_formula: string;
     private readonly result_value: number;
     private readonly explicitTexPareses: boolean;
@@ -64,11 +58,9 @@ export class ArithmeticExpression implements Expression {
                 private readonly rank: OperationRank,
                 private readonly associative: boolean,
                 explicitTexPareses: boolean,
-                symbol: string,
                 private readonly left: Expression,
                 private readonly right: Expression) {
         this.explicitTexPareses = explicitTexPareses;
-        this.formula = this.buildFormula(symbol);
         this.tex_formula = this.buildTex(tex);
         this.result_value = calc(left.getResult(), right.getResult());
     }
@@ -77,18 +69,8 @@ export class ArithmeticExpression implements Expression {
         return this.result_value;
     }
 
-    private buildFormula(symbol: string): string {
-        const leftStr = ArithmeticExpression.buildOperandFormulaStr(this.left, this.needLeftParenthesis());
-        const rightStr = ArithmeticExpression.buildOperandFormulaStr(this.right, this.needRightParenthesis());
-        return `${leftStr} ${symbol} ${rightStr}`
-    }
-
     private needLeftParenthesis(): boolean {
         return this.rank > this.left.getRank()
-    }
-
-    static buildOperandFormulaStr(expr: Expression, toEmbrace: boolean): string {
-        return toEmbrace ? `(${expr.getFormula()})` : `${expr.getFormula()}`;
     }
 
     static buildOperandTexStr(expr: Expression, toEmbrace: boolean): string {
@@ -97,10 +79,6 @@ export class ArithmeticExpression implements Expression {
 
     private needRightParenthesis(): boolean {
         return this.rank > this.right.getRank() || (!this.associative && this.rank === this.right.getRank())
-    }
-
-    getFormula(): string {
-        return this.formula;
     }
 
     getRank(): OperationRank {
@@ -124,22 +102,15 @@ export class ArithmeticExpression implements Expression {
 
 export class FunctionExpression implements Expression {
 
-    private readonly formula: string;
     private readonly tex_formula: string;
     private readonly result_value: number;
 
     constructor(
             calc: (arg: Result) => Result,
             buildTex: (arg: string) => string,
-            buildFormula: (arg: string) => string,
             private readonly arg: Expression) {
-        this.formula = buildFormula(arg.getTex());
         this.tex_formula = buildTex(arg.getTex());
         this.result_value = calc(arg.getResult());
-    }
-
-    getFormula(): string {
-        return this.formula;
     }
 
     getRank(): OperationRank {
--- src/stores/operations.ts
+++ src/stores/operations.ts
@@ -11,37 +11,37 @@ const operationsTable: { [opt: string]: OptParam } = {
     "+": {
         operandsNumber: 2,
         build: function (a: Expression, b: Expression) {
-            return new ArithmeticExpression((a, b) => `${a}+${b}`, (a, b) => a + b, OperationRank.PLUS_MINUS, true, true, "+", a, b)
+            return new ArithmeticExpression((a, b) => `${a}+${b}`, (a, b) => a + b, OperationRank.PLUS_MINUS, true, true, a, b)
         },
     },
     "-": {
         operandsNumber: 2,
         build: function (a: Expression, b: Expression) {
-            return new ArithmeticExpression((a, b) => `${a}-${b}`, (a, b) => a - b, OperationRank.PLUS_MINUS, false, true, "-", a, b)
+            return new ArithmeticExpression((a, b) => `${a}-${b}`, (a, b) => a - b, OperationRank.PLUS_MINUS, false, true, a, b)
         },
     },
     "*": {
         operandsNumber: 2,
         build: function (a: Expression, b: Expression) {
-            return new ArithmeticExpression((a, b) => `${a}\\times${b}`, (a, b) => a * b, OperationRank.MULT__DIV, true, true, "*", a, b)
+            return new ArithmeticExpression((a, b) => `${a}\\times${b}`, (a, b) => a * b, OperationRank.MULT__DIV, true, true, a, b)
         },
     },
     "/": {
         operandsNumber: 2,
         build: function (a: Expression, b: Expression) {
-            return new ArithmeticExpression((a, b, leftExpr, rightExpr) => `\\frac{${leftExpr.getTex()}}{${rightExpr.getTex()}}`, (a, b) => a / b, OperationRank.MULT__DIV, false, true, "/", a, b)
+            return new ArithmeticExpression((a, b, leftExpr, rightExpr) => `\\frac{${leftExpr.getTex()}}{${rightExpr.getTex()}}`, (a, b) => a / b, OperationRank.MULT__DIV, false, true, a, b)
         },
     },
     "sqrt": {
         operandsNumber: 1,
         build: function (x: Expression) {
-            return new FunctionExpression((x) => Math.sqrt(x), (x) => `\\sqrt{${x}}`, (x) => `sqrt(${x})`, x)
+            return new FunctionExpression((x) => Math.sqrt(x), (x) => `\\sqrt{${x}}`, x)
         },
     },
     "sqr": {
         operandsNumber: 1,
         build: function (x: Expression) {
-            return new FunctionExpression((x) => x * x, (x) => `\\left(${x}\\right)^{2}`, (x) => `(${x}^2)`, x)
+            return new FunctionExpression((x) => x * x, (x) => `\\left(${x}\\right)^{2}`, x)
         },
     }
 };
